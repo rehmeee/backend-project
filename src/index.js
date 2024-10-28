8020bb15c06a7ea0eb04245d0950a0a57bd8ee0f
ScriptSorcererMani <chrehmanali5@gmail.com>
2024-10-28 21:58:07 +0500

    try to connect to the database using a simple approch

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import { DATABASE_NAME } from "./constant";
+
+
+
+
+
+
+
+
+// this is the effi way to connect to the database but this is not a professional approch so dont prefer it most of the times
+/*
+import express from "express";
+const app = express();
+(async () => {
+  try {
+    await mongoose.connect(`${DATABASE_URI}/${DATABASE_NAME}`);
+    app.on("error", (error) => {
+      console.log("error while talking to database", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`the port is listening on the port no  ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("error", error);
+  }
+})();
+*/
